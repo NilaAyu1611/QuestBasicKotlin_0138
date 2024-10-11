a902db7d48a500fefbc0183de603fd50a8fee1fe
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-11 23:15:19 +0700

    Membuat fungsi Contohlist, yang dimana dengan menggunakan list read-only

---
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Collections.kt
@@ -0,0 +1,10 @@
+package com.example.cobamyapplicationkototlin
+
+fun ContohList() {
+    println("=== List ===")         // menyimpan data sesuai urutan penambahannya dan bisa duplikat
+    // List Read-Only, data yang tidak bisa ditambah/diubah
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)          // mencetak elemenya
+
+
+}
