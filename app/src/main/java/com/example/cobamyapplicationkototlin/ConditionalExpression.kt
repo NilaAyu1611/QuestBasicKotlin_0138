8becc787d7a2caffdc443096d34da007989b68ca
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 16:58:26 +0700

    Membuat fungsi ConditonalStatement menggunakan if untuk memeriksa ekspresi bersyarat

--- app/src/main/java/com/example/cobamyapplicationkototlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/ConditionalExpression.kt
@@ -1,2 +1,19 @@
 package com.example.cobamyapplicationkototlin
 
+// === Conditional Expression ===
+// Conditional expression --> ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression")
+
+// If-else --> memeriksa kondisi & melakukan sesuatu jika T & F
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+
+
+}
