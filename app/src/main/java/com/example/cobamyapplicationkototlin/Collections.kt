f27c394af4408767295b44f43340c20df6cab3ea
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 08:52:33 +0700

    Membuat fungsi ContohMap, dengan Map Mutable

--- app/src/main/java/com/example/cobamyapplicationkototlin/Collections.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Collections.kt
@@ -64,4 +64,22 @@ fun ContohMap() {
     val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
     println(readOnlyShape)
 
+    // Map Mutable  --> dapat diubah
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+
+
 }
