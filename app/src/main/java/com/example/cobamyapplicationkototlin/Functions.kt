534d373d0a9f53c33c74c94a16836fb40d6234db
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 19:08:38 +0700

    Membuat Function dengan parameter dan tanpa paramter

--- app/src/main/java/com/example/cobamyapplicationkototlin/Functions.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Functions.kt
@@ -1,3 +1,16 @@
 package com.example.cobamyapplicationkototlin
 
+// Fungsi tanpa parameter --> tidak menerima input apapun
+fun withoutParameter() {
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+// Fungsi dengan parameter --> menerima 1 parameter (name)
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
 
