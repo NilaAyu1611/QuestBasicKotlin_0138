bb029ec6ca595c85af9e689a3b97742e46d9c274
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 08:45:27 +0700

    Membuat fungsi ContohMap, dengan Map Read-Only

--- app/src/main/java/com/example/cobamyapplicationkototlin/Collections.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Collections.kt
@@ -56,3 +56,12 @@ fun ContohSet() {
 
 
 }
+
+fun ContohMap() {
+    println()
+    println("=== Map ===")      // Menyimpan data dengan berpasangan, kunci-value
+    // Map Read-Only  --> tidak dapat diubah
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+}
