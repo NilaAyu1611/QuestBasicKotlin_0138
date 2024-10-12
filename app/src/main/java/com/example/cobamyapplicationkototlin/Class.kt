ab3346cd4018e3e0521d74e0f5021d72707bf061
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 09:14:39 +0700

    Membuat Properti

--- app/src/main/java/com/example/cobamyapplicationkototlin/Class.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Class.kt
@@ -1,4 +1,21 @@
 package com.example.cobamyapplicationkototlin
 
+class Motor()
+                                                // Membuat objek dari sebuah kelas, dengan properti yang dideklarasi dengan val (tidak dapat diubah) dan var (dapat diubah)
+class Contact(val id: Int, var email:String)
 
+fun main() {
+    val contact = Contact(1, "mary@gmail.com")
+
+// Prints the value of the property: email
+    println(contact.email)
+
+
+// Updates the value of the property: email
+    contact.email = "jane@gmail.com"
+
+// Prints the new value of the property: email
+    println(contact.email)
+
+}
 
