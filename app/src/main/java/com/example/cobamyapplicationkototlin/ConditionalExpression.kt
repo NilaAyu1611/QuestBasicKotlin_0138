a26eb62fdf8f7e5aa72b55041ff0787a3a38fae4
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 17:24:48 +0700

    Membuat fungsi Perulangan dengan struktur for dan while

--- app/src/main/java/com/example/cobamyapplicationkototlin/ConditionalExpression.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/ConditionalExpression.kt
@@ -37,3 +37,31 @@ fun ConditionalStatement() {
         println("Invalid number")
     }
 }
+
+
+// Loops ---> ada 2 struktur yaitu for loop dan while loop
+fun Perulangan() {
+    println()
+    println("=== Perulangan ===")
+
+    for (i in 1..5) {               // menggunakan for loop --> mengulangi serangkaian nilai yang telah ditentukan
+        println("Perulangan ke-$i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("Car type: $car")
+    }
+
+    var x = 1                           // while --> untuk mengeksekusi blok kode selama ekspresi kondisional bernilai T
+    while (x <= 5) {
+        println("Perulangan ke-$x")
+        x++
+    }
+
+    var y = 1                          // do-while --> mengeksekusi blok kode dulu baru memeriksa ekspresi kondisional.
+    do {
+        println("Perulangan ke-$y")
+        y++
+    } while (y <= 5)
+}
