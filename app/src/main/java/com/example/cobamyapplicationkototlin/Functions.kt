493bdaf2929d422090dc94480798e0251bcb8269
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 19:10:30 +0700

    Membuat fungsi namedargument, default paramter,dan return serta untuk memanggil fungsi sebelumnya

--- app/src/main/java/com/example/cobamyapplicationkototlin/Functions.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/Functions.kt
@@ -14,3 +14,32 @@ fun withParameter(name: String) {
 }
 
 
+// === Named argument ===
+// Named argument --> cara untuk memanggil fungsi dengan menyebutkan nama parameter
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+
+// === Default parameter values ===
+// Default parameter value --> cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = "Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+// Fungsi dengan return value
+fun withReturn(panjang: Int, lebar: Int): Int {
+    return panjang * lebar
+}
+
+fun main() {                        // Memanggil semua fungsi yang telah dideklarasikan
+    withoutParameter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
+
