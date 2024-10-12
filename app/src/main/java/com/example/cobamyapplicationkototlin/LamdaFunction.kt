dce4e21929ab58dca00bac7c269af37b29ec9127
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 22:09:48 +0700

    Membuat fungsi uppercaseString dengan menggunakn lamda expression

--- app/src/main/java/com/example/cobamyapplicationkototlin/LamdaFunction.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/LamdaFunction.kt
@@ -1,2 +1,21 @@
 package com.example.cobamyapplicationkototlin
 
+// === Lambda Functions ===
+// Lambda Functions --> fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Functions menggunakan tanda panah -> untuk memisahkan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {           // Fungsi standar
+    return string.uppercase()
+}
+
+                // |
+                // |
+                // V
+
+//Dapat ditulis dalam Lamda Expression (lebih ringkas)
+fun main() {
+    uppercaseString("hello")
+    println({ string: String -> string.uppercase() }("hello"))      // Lamda Expression
+// HELLO
+}
