cc3ef824375997a174d8f1de65eeca8e369b7b53
NilaAyu1611 <nilaayucw@gmail.com>
2024-10-12 18:42:21 +0700

    Mendeklarasikan Data Class dengan fungsi dasar equals (perbandingan objek)

--- app/src/main/java/com/example/cobamyapplicationkototlin/DataClass.kt
+++ app/src/main/java/com/example/cobamyapplicationkototlin/DataClass.kt
@@ -1,4 +1,24 @@
 package com.example.cobamyapplicationkototlin
 
+// === Data Classes ===
+// Data classes --> class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString(),
+// Data class juga menyediakan fungsi copy() untuk membuat  salinan objek dengan beberapa perubahan.
 
+data class DataClass(           // deklarasikan kelas data
+    val id: Int,
+    var email: String
+)
+
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+// Fungsi equals -->  ( == ) membandingkan instance dari suatu kelas.
+    val data2 = DataClass(1, "makan@gmaul.com")
+    println(data == data2) // false
+
+
+}
 
